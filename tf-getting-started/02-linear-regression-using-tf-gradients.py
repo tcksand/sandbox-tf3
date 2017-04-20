e6bedc036aa169ba977c96c0e77b0db1854bf0be
tcksand <tckim0144@gmail.com>
2017-04-20 21:50:46 +0900

    bug fix

--- tf-getting-started/02-linear-regression-using-tf-gradients.py
+++ tf-getting-started/02-linear-regression-using-tf-gradients.py
@@ -1,17 +1,14 @@
+# linear regression using tf.gradients
 import tensorflow as tf
 
-### linear regression using tf.gradients
 W = tf.Variable([.3], tf.float32)
 b = tf.Variable([-.3], tf.float32)
+
 x = tf.placeholder(tf.float32)
+y = tf.placeholder(tf.float32)
+
 linear_model = W * x + b
 
-init = tf.global_variables_initializer()
-sess.run(init)
-
-#print(sess.run(linear_model, feed_dict={x:[1.,2.,3.,4.]}))
-
-y = tf.placeholder(tf.float32)
 squared_deltas = tf.square(linear_model - y)
 loss = tf.reduce_sum(squared_deltas)
 dW, db = tf.gradients(loss, [W, b])
@@ -20,11 +17,17 @@ fixW = tf.assign(W, W - learning_rate * dW)
 fixb = tf.assign(b, b - learning_rate * db)
 train = [fixW, fixb]
 
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+# print(sess.run(linear_model, feed_dict={x:[1.,2.,3.,4.]}))
+
 for step in range(2000):
 
     cur_train, cur_loss = sess.run([train,loss],feed_dict={x:[1,2,3,4], y:[0,-1,-2,-3]} )
 
-    if step % 10 == 0:
+    if step % 100 == 0:
         print(step, cur_loss, sess.run(W), sess.run(b))
 
 
